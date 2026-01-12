22b8e9fe107e86442c2b179d23496c40b6a5bcf5
hlogi10122 <79769303+hlogi10122@users.noreply.github.com>
2026-01-12 11:02:34 +0200

    Added movies to Api and database

--- src/main/java/com/movie/movie_app_backend/Controllers/MoviesController.java
+++ src/main/java/com/movie/movie_app_backend/Controllers/MoviesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
